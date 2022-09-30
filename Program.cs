01c3b124aef8edde6af43c3b4683ad96f6ec4443
Vincent Houle <2241013etudiant@gmail.com>
2022-09-30 14:56:13 -0400

    Nouveau

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int choix = choisirOptionMenu();
+
+            Console.WriteLine("Vous avez choisi l'option "+choix);
+        }
+
+        static int choisirOptionMenu()
+        {
+            int choix = 0;
+            string menu = "Choisir 1 pour Jeu du Pendu, 2 pour Tic Tac Toe, 3 pour Quitter";
+            bool valide =false;
+
+            while (!valide)
+            {
+
+                Console.Clear();
+                Console.WriteLine(menu);
+                int.TryParse(Console.ReadLine(), out choix);
+
+                if(choix ==1 || choix==2 || choix == 3)
+                {
+                    valide = true;
+                }
+
+            }
+
+
+            return choix;
         }
     }
 }
